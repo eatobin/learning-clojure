88a2b1805373e713e9d6c9c3130d62d88e79ae64
Eric Tobin <eatobin@gmail.com>
2015-05-30 23:32:47 -0500

    ch 1 progress

--- src/learning_clojure/living-clojure/ch-1.clj
+++ src/learning_clojure/living-clojure/ch-1.clj
@@ -23,10 +23,30 @@ nil
 (first (rest (rest '(:rabbit :pocket-watch :marmalade :door))))
 (first (rest (rest (rest '(:rabbit :pocket-watch :marmalade :door)))))
 (first (rest (rest (rest (rest '(:rabbit :pocket-watch :marmalade :door))))))
-
-
-
-
-
-
-
+(cons 5 '())
+(cons 5 nil)
+(cons 4 (cons 5 nil))
+(cons 3 (cons 4 (cons 5 nil)))
+(cons 2 (cons 3 (cons 4 (cons 5 nil))))
+'(1 2 3 4 5)
+(list 1 2 3 4 5)
+[:jar1 1 2 3 :jar2]
+(first [:jar1 1 2 3 :jar2])
+(rest [:jar1 1 2 3 :jar2])
+(nth [:jar1 1 2 3 :jar2] 0)
+(nth [:jar1 1 2 3 :jar2] 4)
+(last [:rabbit :pocket-watch :marmalade])
+(last '(:rabbit :pocket-watch :marmalade))
+(count [1 2 3 4])
+(conj [:toast :butter] :jam)
+(conj [:toast :butter] :jam :honey)
+(conj '(:toast :butter) :jam)
+(conj '(:toast :butter) :jam :honey)
+{:jam1 "strawberry" :jam2 "blackberry"}
+{:jam1 "strawberry", :jam2 "blackberry"}
+(get {:jam1 "strawberry", :jam2 "blackberry"} :jam2)
+(get {:jam1 "strawberry", :jam2 "blackberry"} :jam3 "not found")
+(:jam2 {:jam1 "strawberry", :jam2 "blackberry" :jam3 "marmalade"})
+(keys {:jam1 "strawberry", :jam2 "blackberry", :jam3 "marmalade"})
+(vals {:jam1 "strawberry", :jam2 "blackberry", :jam3 "marmalade"})
+(assoc {:jam1 "red" :jam2 "black"} :jam1 "orange")
