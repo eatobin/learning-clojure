2c4c7c60005bd37a8e8f050dab8fd58bfacdc656
Eric Tobin <eatobin@gmail.com>
2015-06-01 12:22:24 -0500

    ch 2 progress

--- src/learning_clojure/living-clojure/ch-2.clj
+++ src/learning_clojure/living-clojure/ch-2.clj
@@ -35,17 +35,27 @@
 
 (every? odd? [1 3 5])
 (every? odd? [1 2 3 4 5])
-;; page 31
 
+(defn drinkable? [x]
+  (= x :drinkme))
+(drinkable? :drinkme)
+(drinkable? :poison)
+(every? drinkable? [:drinkme :drinkme])
+(every? drinkable? [:drinkme :poison])
+fn [x] (= x :drinkme)
+(every? (fn [x] (= x :drinkme) :toast) [:drinkme :drinkme])
+(every? #(= % :drinkme) [:drinkme :drinkme])
+(not-any? #(= % :drinkme) [:drinkme :poison])
+(not-any? #(= % :drinkme) [:poison :poison])
 
-
-
-
-
-
-
-
-
+(some #(> % 3) [1 2 3 4 5])
+(#{1 2 3 4 5} 3)
+(#{1 2 3 4 5} 9)                                            ;; nil
+(some #{3} [1 2 3 4 5])
+(some #{4 5} [1 2 3 4 5])
+(some #{8 9} [1 2 3 4 5])                                   ;; nil
+(some #{nil} [nil nil nil])
+(some #{false} [false false false])
 
 
 
