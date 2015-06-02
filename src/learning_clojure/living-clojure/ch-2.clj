00c6ef9ead5be0384336ebc285451c6e2a3e5f10
Eric Tobin <eatobin@gmail.com>
2015-06-01 22:08:30 -0500

    ch 2 progress

--- src/learning_clojure/living-clojure/ch-2.clj
+++ src/learning_clojure/living-clojure/ch-2.clj
@@ -189,7 +189,48 @@ fn [x] (= x :drinkme)
     "unknown"))
 ;; -> "unknown"
 
-;; page 36
+(defn grow [name direction]
+  (if (= direction :small)
+    (str name " is growing smaller")
+    (str name " is growing bigger")))
+;; -> #'user/grow
+(grow "Alice" :small)
+;; -> "Alice is growing smaller"
+(grow "Alice" :big)
+;; -> "Alice is growing bigger"
+(partial grow "Alice")
+;; -> #<core$partial$fn__4228 clojure.core$partial$fn__4228@1759817d>
+((partial grow "Alice") :small)
+;; -> "Alice is growing smaller"
+
+(defn toggle-grow [direction]
+  (if (= direction :small) :big :small))
+;; -> #'user/toggle-grow
+(toggle-grow :big)
+;; -> :small
+(toggle-grow :small)
+;; -> :big
+(defn oh-my [direction]
+  (str "Oh My! You are growing " direction))
+;; -> #'user/oh-my
+(oh-my (toggle-grow :small))
+;; -> "Oh My! You are growing :big"
+;; Or, we could use comp to create a function that was the composition of the two:
+(defn surprise [direction]
+  ((comp oh-my toggle-grow) direction))
+(surprise :small)
+;; -> "Oh My! You are growing :big"
+
+(defn adder [x y]
+  (+ x y))
+;; -> #'user/adder
+(adder 3 4)
+;; -> 7
+(def adder-5 (partial adder 5))
+;; -> #'user/adder-5
+(adder-5 10)
+;; -> 15
+
 
 
 
