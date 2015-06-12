fdef31dedab7672e5cd302b30cfac696640840b1
Eric Tobin <eatobin@gmail.com>
2015-06-11 23:44:17 -0500

    atom cache!!

---
+++ src/learning_clojure/atomcache.clj
@@ -0,0 +1,55 @@
+; START:create
+(defn create
+  []
+  (atom {}))
+; END:create
+
+; START:get
+(defn get
+  [cache key]
+  (@cache key))
+; END:get
+; START:put
+(defn put
+  ([cache value-map]
+     (swap! cache merge value-map))
+  ([cache key value]
+     (swap! cache assoc key value)))
+; END:put
+; START:usage
+(def ac (create))
+(put ac :first "Eric")
+; {:first "Eric"}
+(println (str "Item: " (get ac :first)))
+; Item: Eric
+; nil
+(put ac :first "Scott")
+; {:first "Scott"}
+(println (str "Item: " (get ac :first)))
+; Item: Scott
+;nil
+(put ac :last "Scott")
+; {:last "Scott", :first "Scott"}
+solutions.atom-cache=> (put ac :last "Tobin")
+; {:last "Tobin", :first "Scott"}
+(put ac {:middle "None" :age 66})
+; {:middle "None", :age 66, :last "Tobin", :first "Scott"}
+; END:usage
+
+(def m (atom {:like {:this {:nested (atom {:value 5})}}}))
+
+@(get-in @m [:like :this :nested])
+; => {:value 5}
+
+(get-in @(get-in @m [:like :this :nested]) [:value])
+; => 5
+
+(def k (atom {:0 {:first "Eric" :last "Tobin"} :1 {:first "Scott" :last "Jones"}}))
+(get-in @k [:1 :first])
+; "Scott"
+
+(defn get-in
+  ([m ks]
+     (reduce get @m ks)))
+(get-in k [:1 :first])
+; "Scott"
