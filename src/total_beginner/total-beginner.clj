f16f459e5b49d157cd518673c812b8ae21eb057b
Eric Tobin <eatobin@gmail.com>
2015-06-15 20:05:41 -0500

    work on my library

--- src/total_beginner/total-beginner.clj
+++ src/total_beginner/total-beginner.clj
@@ -1,6 +1,6 @@
-(def books [{:title "Book One" :author "Joe Blow" :person "Person One"}
+(def books (atom [{:title "Book One" :author "Joe Blow" :person "Person One"}
             {:title "Book Two" :author "Joe Two" :person nil}
-            {:title "The Bible" :author "G-d" :person "Person One"}])
+                  {:title "The Bible" :author "G-d" :person "Person One"}]))
 
 (def people [{:name "Person One" :max-books 3}
              {:name "Person Two" :max-books 6}])
@@ -8,17 +8,19 @@
 (filter (comp #{"Book One"} :title) books)
 (filter (comp #{"Person One"} :name) people)
 (filter (comp #{"Person One"} :person) books)
-(defn get-books-for-person [person library]
-  (filter (comp #{person} :person) (deref library)))
+(defn get-books-for-person [person]
+  (filter (comp #{person} :person) (deref books)))
 
 (defn add-person [name max-books]
   (swap! people conj {:name name, :max-books max-books}))
 
-(defn create-books []
-  (def books (atom [])))
-
-(defn create-people []
-  (def people (atom [])))
-
 (defn add-book [title author person]
   (swap! books conj {:title title :author author :person person}))
+
+(defn create-library [name]
+  (def library-name name)
+  (def books (atom []))
+  (def people (atom [])))
+
+(defn get-available-books []
+  (filter (comp nil? :person) (deref books)))
