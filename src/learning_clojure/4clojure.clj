24250ede0c45dbbfbb9e7234956b5068d8b21636
Eric Tobin <eatobin@gmail.com>
2015-08-23 21:50:52 -0500

    4clojure #51

--- src/learning_clojure/4clojure.clj
+++ src/learning_clojure/4clojure.clj
@@ -1,1 +1,20 @@
 (ns learning-clojure.4clojure)
+
+;; 51
+
+(let [[a b & c] ["cat" "dog" "bird" "fish"]]
+  [a b])
+;; -> ["cat" "dog"]
+
+(let [[a b & c] ["cat" "dog" "bird" "fish"]]
+  c)
+;; -> ("bird" "fish")
+
+;The other type of destructuring technique is to use the :as keyword. This takes the
+;whole destructuring argument and binds it to a name:
+
+(let [[a b :as x] ["cat" "dog" "bird" "fish"]]
+  x)
+;; -> ["cat" "dog" "bird" "fish"]
+
+(= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] [1 2 3 4 5]] [a b c d]))
