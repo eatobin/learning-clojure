406c64392a14dbc42871b8024f07b9cc46ac31b9
Eric Tobin <eatobin@gmail.com>
2015-08-31 16:12:03 -0500

    well g progress

--- src/learning_clojure/well-ground-java.clj
+++ src/learning_clojure/well-ground-java.clj
@@ -3,9 +3,10 @@
 (defn const-fun1 [_] 1)
 (defn ident-fun [x] x)
 (defn list-maker-fun [x f]
-  (map (fn [z] (let [w z]
-                 (list (f w) w)
-                 )) x))
+  (map (fn [z]
+         (let [w z]
+           (list (f w) w)))
+       x))
 (const-fun1 "hi")
 (ident-fun "hi")
 (list-maker-fun ["a" "b"] const-fun1)
@@ -13,6 +14,15 @@
 
 (defn make-adder [x]
   (let [y x]
-    (fn [z] (+ y z))))
+    (fn [z]
+      (+ y z))))
 (def add2 (make-adder 2))
 (add2 24)
+
+(defn make-adder2 [a]
+  (fn [z]
+    (+ a z)))
+
+(def add3 (make-adder2 3))
+(add3 3)
+(add3 24)
