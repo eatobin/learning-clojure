b47f48f9e8b72e8f2bf070b6badb00ae8987dd07
Eric Tobin <eatobin@gmail.com>
2015-11-18 11:29:39 -0600

    revised haskell-clojure

--- src/learning_clojure/clojure-haskell.clj
+++ src/learning_clojure/clojure-haskell.clj
@@ -62,3 +62,10 @@
 ;-- 4662110665499438 True
 ;-- 645937 True
 ;-- 1859 True
+
+;type Peg = String
+;type Move = (Peg, Peg)
+
+;hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+;hanoi 1 p1 _ p3 = [(p1, p3)]
+;hanoi n p1 p2 p3 = (hanoi (n - 1) p1 p3 p2) ++ (hanoi 1 p1 p2 p3) ++ (hanoi (n - 1) p2 p1 p3)
