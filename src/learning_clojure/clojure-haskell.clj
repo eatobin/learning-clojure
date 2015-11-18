8c1ba4a2a93f3bd2b60264a640b4f199ad3655b5
Eric Tobin <eatobin@gmail.com>
2015-11-18 14:53:01 -0600

    broken hanoi

--- src/learning_clojure/clojure-haskell.clj
+++ src/learning_clojure/clojure-haskell.clj
@@ -63,8 +63,23 @@
 ;-- 645937 True
 ;-- 1859 True
 
+;(defn hanoi [n p1 p2 p3]
+;  (if (= n 1)
+;    (println (format "Move from %s to %s" p1 p3))
+;    (do
+;      (hanoi (dec n) p1 p3 p2)
+;      (println (format "Move fromX %s to %s" p1 p3))
+;      (recur (dec n) p2 p1 p3))))
+
 (defn hanoi [n p1 p2 p3]
-  )
+  (loop [ln n lp1 p1 lp2 p2 lp3 p3
+         a []]
+    (if (= ln 0)
+      a
+      (hanoi (dec n) p1 p3 p2
+             (recur 1 p1 p2 p3
+                    (when (= n 1)
+                      (conj a (list p1 p3))))))))
 
 ;type Peg = String
 ;type Move = (Peg, Peg)
