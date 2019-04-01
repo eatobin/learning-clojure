6e57529eee4f1dcb1c18f60e59bcfb10ec87ca60
Eric Tobin <eatobin@gmail.com>
2019-04-01 14:32:25 -0500

    ds defn!

--- src/learning_clojure/spec.clj
+++ src/learning_clojure/spec.clj
@@ -1,6 +1,5 @@
 (ns learning-clojure.spec
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]
             [clojure.spec.gen.alpha :as sgen]
             [clojure.test.check :as tc]
@@ -11,10 +10,52 @@
 
 ;; https://clojure.org/guides/spec
 
-(ds/defn adder :- int?
+(ds/defn adder :- double?
   "This is a test comment"
   [x :- int?]
-  (inc x))
+  (+ x 0.88))
+(adder 44)
+
+(defn ranged-rand
+  "Returns random int in range start <= rand < end"
+  [start end]
+  (+ start (long (rand (- end start)))))
+
+(s/fdef ranged-rand
+        :args (s/and (s/cat :start int? :end int?)
+                     #(< (:start %) (:end %)))
+        :ret int?
+        :fn (s/and #(>= (:ret %) (-> % :args :start))
+                   #(< (:ret %) (-> % :args :end))))
+
+(defn digits
+  "Takes just an int and returns the set of its digit characters."
+  [just-an-int]
+  (into #{} (str just-an-int)))
+
+(s/fdef digits
+        :args (s/cat :just-an-int int?)
+        :ret (s/coll-of char? :kind set? :min-count 1))
+
+(ostest/instrument)
+
+
+
+(ranged-rand 8 50)
+;(ranged-rand 8 5)
+(stest/check `ranged-rand)
+
+(digits 789)
+;(digits 7.8)
+(stest/check `digits)
+
+(gen/generate (s/gen int?))
+(gen/sample (s/gen string?))
+
+(s/def ::suit #{:club :diamond :heart :spade})
+(gen/sample (s/gen #{:club :diamond :heart :spade}))
+
+(s/exercise-fn `ranged-rand)
 
 (defn ascending?
   "clojure.core/sorted? doesn't do what we might expect, so we write our
@@ -51,42 +92,3 @@
 (gen/sample vec-and-elem 8)
 
 (sgen/generate (s/gen int?))
-
-(defn ranged-rand
-  "Returns random int in range start <= rand < end"
-  [start end]
-  (+ start (long (rand (- end start)))))
-
-(s/fdef ranged-rand
-        :args (s/and (s/cat :start int? :end int?)
-                     #(< (:start %) (:end %)))
-        :ret int?
-        :fn (s/and #(>= (:ret %) (-> % :args :start))
-                   #(< (:ret %) (-> % :args :end))))
-
-(defn digits
-  "Takes just an int and returns the set of its digit characters."
-  [just-an-int]
-  (into #{} (str just-an-int)))
-
-(s/fdef digits
-        :args (s/cat :just-an-int int?)
-        :ret (s/coll-of char? :kind set? :min-count 1))
-
-(ostest/instrument)
-
-(ranged-rand 8 50)
-;(ranged-rand 8 5)
-(stest/check `ranged-rand)
-
-(digits 789)
-;(digits 7.8)
-(stest/check `digits)
-
-(gen/generate (s/gen int?))
-(gen/sample (s/gen string?))
-
-(s/def ::suit #{:club :diamond :heart :spade})
-(gen/sample (s/gen #{:club :diamond :heart :spade}))
-
-(s/exercise-fn `ranged-rand)
