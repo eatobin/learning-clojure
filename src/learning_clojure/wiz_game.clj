bacd3c86a5bb5451f1d210b419eaa52852553bcf
Eric Tobin <eatobin@gmail.com>
2019-04-16 17:14:15 -0500

    progress

--- src/learning_clojure/wiz_game.clj
+++ src/learning_clojure/wiz_game.clj
@@ -6,5 +6,14 @@
                                there is a well in front of you.)
             'attic       '(you are in the attic.
                                there is a giant welding torch in the corner.)})
+
 (defn describe-location [location nodes]
   (get nodes location))
+
+(def edges {'living-room '((garden west door)
+                            (attic upstairs ladder))
+            'garden      '(living-room east door)
+            'attic       '(living-room downstairs ladder)})
+
+(defn describe-path [edge]
+  (list 'there 'is 'a (first (rest (rest edge))) 'going (first (rest edge)) 'from 'here))
