deb2998b8d64f5e20c3b7bccb2b2b881c9cf212a
Eric Tobin <eatobin@gmail.com>
2019-06-02 20:41:57 -0500

    ch2

---
+++ src/learning_clojure/schemer/ch_2.clj
@@ -0,0 +1,11 @@
+(ns learning-clojure.schemer.ch-2
+  (:require [learning-clojure.schemer.ch-1 :as ch1]))
+
+(def lat?
+  (fn [l]
+    (cond
+      (ch1/null? l) true
+      (and (seq? l)
+           (ch1/atom? (first l)))
+      (lat? (rest l))
+      true false)))
