bf1acca2110c0c2398456c1402f9e3fd122a5d72
Eric Tobin <eatobin@gmail.com>
2019-06-18 14:31:11 -0500

    insertL!!

--- src/learning_clojure/schemer/ch3.clj
+++ src/learning_clojure/schemer/ch3.clj
@@ -26,3 +26,11 @@
       true (cond
              (= (first lat) old) (cons (first lat) (cons new (rest lat)))
              true (cons (first lat) (insertR new old (rest lat)))))))
+
+(def insertL
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      true (cond
+             (= (first lat) old) (cons new lat)
+             true (cons (first lat) (insertL new old (rest lat)))))))
