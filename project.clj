d6da40d9ac4c5a0f5daf3694af5299c4a1ef90a8
Eric Tobin <eatobin@gmail.com>
2019-08-02 15:35:51 -0500

    New lein deps

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[circleci/clj-yaml "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/test.check "0.9.0"]
+                 [circleci/clj-yaml "0.6.0"]
                  [orchestra "2019.02.06-1"]
                  [net.danielcompton/defn-spec-alpha "0.1.0"]]
   ; :main ^:skip-aot learning-clojure.core
