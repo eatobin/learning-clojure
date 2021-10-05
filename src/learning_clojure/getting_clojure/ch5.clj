1865a38f7b5c073f2e3999e3de14de2b466a1e59
Eric Tobin <eatobin@gmail.com>
2021-10-05 15:33:46 -0500

    progress

--- src/learning_clojure/getting_clojure/ch5.clj
+++ src/learning_clojure/getting_clojure/ch5.clj
@@ -55,3 +55,25 @@
   (format "$%.2f" (double (compute-royalties {:price 10.00 :published 1901})))
   (format "$%.2f" (double (compute-royalties {:published 1950 :price 20.00})))
   (format "$%.2f" (double (compute-royalties {:price 30.00 :published 2001}))))
+
+(def books [{:title "Pride and Prejudice" :author "Austen" :genre :romance}
+            {:title "World War Z" :author "Brooks" :genre :zombie}])
+
+;we could certainly create a multimethod based on the genre:
+;; Remember you can use keys like :genre like functions on maps.
+(defmulti book-description :genre)
+(defmethod book-description :romance [book]
+  (str "The heart warming new romance by " (:author book)))
+(defmethod book-description :zombie [book]
+  (str "The heart consuming new zombie adventure by " (:author book)))
+(defmethod book-description :zombie-romance [book]
+  (str "The heart warming and consuming new romance by " (:author book)))
+
+;But what if much later someone comes up with a new genre?
+(def ppz {:title  "Pride and Prejudice and Zombies"
+          :author "Grahame-Smith"
+          :genre  :zombie-romance})
+
+(comment
+  (map book-description books)
+  (book-description ppz))
