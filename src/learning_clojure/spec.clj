cad8195765f1a749258bc9a0c7852fb9faf1bf65
Eric Tobin <eatobin@gmail.com>
2021-11-21 22:53:01 -0600

    fixed kondo

--- src/learning_clojure/spec.clj
+++ src/learning_clojure/spec.clj
@@ -12,9 +12,13 @@
 
 ;; https://clojure.org/guides/spec
 
+(declare my-inc a adder x v)
+
 (defn-spec my-inc integer?
            [a integer?] ; Each argument is followed by its spec.
            (+ a 1))
+
+(my-inc 44)
 
 (ds/defn adder :- double?
          "This is a test comment"
