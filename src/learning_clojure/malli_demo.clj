4e47c2561661a7fd070aa40eafd6f418d387368f
Eric Tobin <eatobin@gmail.com>
2023-10-02 15:47:19 -0500

    more malli

--- src/learning_clojure/malli_demo.clj
+++ src/learning_clojure/malli_demo.clj
@@ -20,4 +20,30 @@
   (kukka 1 2)
   (kikka "1")
   (kukka "1")
-  (kikka 1.0))
+  (kikka 1.0)
+  (kikka 1))
+
+(dev/stop!)
+
+(defn plus1 [x] (inc x))
+(m/=> plus1 [:=> [:cat :int] [:int {:max 6}]])
+
+(dev/start!)
+; =prints=> ..instrumented #'user/plus1
+; =prints=> started instrumentation
+
+(plus1 "6")
+; =throws=> :malli.core/invalid-input {:input [:cat :int], :args ["6"], :schema [:=> [:cat :int] [:int {:max 6}]]}
+
+(plus1 6)
+; =throws=> :malli.core/invalid-output {:output [:int {:max 6}], :value 9, :args [8], :schema [:=> [:cat :int] [:int {:max 6}]]}
+
+(m/=> plus1 [:=> [:cat :int] :int])
+; =prints=> ..instrumented #'user/plus1
+
+(plus1 6)
+; => 7
+
+(dev/stop!)
+; =prints=> ..unstrumented #'user/plus1
+; =prints=> stopped instrumentation
